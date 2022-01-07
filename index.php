370f8e06df32ccc41249e09b7ded223efbf82dfc
phan111 <chatpimookt@gmail.com>
2022-01-07 10:58:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -77,8 +77,9 @@
     <body>
         
         <div id="wrap">
-          <form action="" autocomplete="on">
-          <input id="search" name="search" type="text" placeholder="What're we looking for ?"><input id="search_submit" value="Rechercher" type="submit">
+          <form action="#" method="get">
+            <input id="search" name="search" type="text" placeholder="กรอกหมายเลขทะเบียนรถ">
+            <input id="search_submit" value="Rechercher" type="submit">
           </form>
         </div>
         

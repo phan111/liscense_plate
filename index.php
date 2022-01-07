5a3cc506255fb62c28e3fdfe6a01db65448991ae
phan111 <chatpimookt@gmail.com>
2022-01-07 09:46:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,5 +13,29 @@ error_reporting(E_ALL);
     </body>
 </html>
 <?php
+require __DIR__ . '/vendor/autoload.php';
+//Reading data from spreadsheet.
+$client = new \Google_Client();
+$client->setApplicationName('Google Sheets and PHP');
+$client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+$client->setAccessType('offline');
+$client->setAuthConfig(__DIR__ . '/credentials.json');
+$service = new Google_Service_Sheets($client);
+$spreadsheetId = "18YfUWVh4UWmD_xbNOy1FaT-mlnjjzgo8ov-dfeIsCWE"; //It is present in your URL
+$get_range = "A1:K";
+//Request to get data from spreadsheet.
+$response = $service->spreadsheets_values->get($spreadsheetId, $get_range);
+$values = $response->getValues();
 
+echo "<pre>";
+print_r($values);
+if(!empty($_GET)){
+    foreach($values AS $key){
+        //print_r($key[7]);
+        //print_r(strpos($key[7], $search));
+        if(strpos($key[7], $_GET['search']) !== FALSE && strpos($key[10], "ไม่") === FALSE){
+            print_r($key);
+        }
+    }
+}
 ?>

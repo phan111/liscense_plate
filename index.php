d0f2ee91361b23e1afe981b578807979e028b63e
phan111 <chatpimookt@gmail.com>
2022-01-07 09:44:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
 <html>
     <head></head>
     <body>
@@ -8,33 +13,5 @@
     </body>
 </html>
 <?php
-ini_set("memory_limit","512M");
-echo ini_get("memory_limit");
-error_reporting(E_ALL);
-require __DIR__ . '/vendor/autoload.php';
-//Reading data from spreadsheet.
-$client = new \Google_Client();
-$client->setApplicationName('Google Sheets and PHP');
-$client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
-$client->setAccessType('offline');
-$client->setAuthConfig(__DIR__ . '/credentials.json');
-$service = new Google_Service_Sheets($client);
-$spreadsheetId = "18YfUWVh4UWmD_xbNOy1FaT-mlnjjzgo8ov-dfeIsCWE"; //It is present in your URL
-$get_range = "A1:K";
-//Request to get data from spreadsheet.
-$response = $service->spreadsheets_values->get($spreadsheetId, $get_range);
-$values = $response->getValues();
 
-/*echo "<pre>";
-if(!empty($_GET)){
-    echo "GET";
-    /*foreach($values AS $key){
-        //print_r($key[7]);
-        //print_r(strpos($key[7], $search));
-        if(strpos($key[7], $_GET['search']) !== FALSE && strpos($key[10], "ไม่") === FALSE){
-            print_r($key);
-        }*/
-    print_r($values);
-    }
-}*/
 ?>

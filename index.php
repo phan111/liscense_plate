b1d362b6c4d01d3b4cbab503c9a823d136fcf8b3
phan111 <chatpimookt@gmail.com>
2022-01-07 15:35:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,107 +5,67 @@
         <meta name="HandheldFriendly" content="true" />
         <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
         <style>
-            @use postcss-color-function;
-            @use postcss-nested;
-            @import url('https://fonts.googleapis.com/css?family=Raleway:400,700,900');
-
-            /* Base styling */
-
-            body {
-                width: 430px;
-                margin: 0 auto;
-                background-color: #F7CAC9;
-                font-family: 'Raleway', sans-serif;
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+            body{
+              background:#7D26CD;
+              .search-box{
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform:translate(-50%,-50%);
+                background: #ccc;
+                height: 40px;
+                border-radius:40px;
+                padding: 10px;
+                &:hover{
+                  .s-box{
+                    width: 240px;
+                    padding: 0 6px;
+                  }
+                  .s-btn{
+                    background:#fff;
+                  }
+                }
+                .s-btn{
+                  color: #7D26CD;
+                  float:right;
+                  width: 40px;
+                  height: 40px;
+                  transition:.4s;
+                  border-radius:50%;
+                  background: #ccc;
+                  display:flex;
+                  justify-content:center;
+                  align-items:center;
+                  i{
+                    font-size:20px;
+                  }
+                }
+                .s-box{
+                  border: none;
+                  background:none;
+                  outline:none;
+                  float:left;
+                  padding: 0;
+                  color:#7D26CD;
+                  font-size:16px;
+                  transition:.5s;
+                  line-height:40px;
+                  width: 0px;
+                }
+              }
             }
 
-            .search {
-                &__container {
-                    padding-top: 64px;
-                }
-
-                &__title {
-                    font-size: 22px;
-                    font-weight: 900;
-                    text-align: center;
-                    color: #ff8b88;
-                }
-
-                &__input {
-                    width: 100%;
-                    padding: 12px 24px;
-
-                    background-color: transparent;
-                    transition: transform 250ms ease-in-out;
-                    font-size: 14px;
-                    line-height: 18px;
-
-                    color: #575756;
-                    background-color: transparent;
-            /*         background-image: url(http://mihaeltomic.com/codepen/input-search/ic_search_black_24px.svg); */
-
-                  background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath d='M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z'/%3E%3Cpath d='M0 0h24v24H0z' fill='none'/%3E%3C/svg%3E");
-                    background-repeat: no-repeat;
-                    background-size: 18px 18px;
-                    background-position: 95% center;
-                    border-radius: 50px;
-                    border: 1px solid #575756;
-                    transition: all 250ms ease-in-out;
-                    backface-visibility: hidden;
-                    transform-style: preserve-3d;
-
-                    &::placeholder {
-                        color: color(#575756 a(0.8));
-                        text-transform: uppercase;
-                        letter-spacing: 1.5px;
-                    }
-
-                    &:hover,
-                    &:focus {
-                        padding: 12px 0;
-                        outline: 0;
-                        border: 1px solid transparent;
-                        border-bottom: 1px solid #575756;
-                        border-radius: 0;
-                        background-position: 100% center;
-                    }
-                }
-            }
-
-            .credits {
-                &__container {
-                    margin-top: 24px;
-                }
-
-                &__text {
-                    text-align: center;
-                    font-size: 13px;
-                    line-height: 18px;
-                }
-
-                &__link {
-                    color: #ff8b88;
-                    text-decoration: none;
-                    transition: color 250ms ease-in;
-
-                    &:hover,
-                    &:focus {
-                        color: color(#ff8b88 blackness(+25%));
-                    }
-                }
-            }
         </style>
         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     </head>
     <body>
-        <div class="search__container">
-            <p class="search__title">
-                Go ahead, hover over search
-            </p>
-            <input class="search__input" type="text" placeholder="Search">
-        </div>
 
-        <div class="credits__container">
-            <p class="credits__text">Background color: Pantone Color of the Year 2016 <a href="https://www.pantone.com/color-of-the-year-2016" class="credits__link">Rose Quartz</a></p>
+        <div class="search-box">
+          <input class="s-box" type="text" name="search" placeholder="Type keyword">
+          <a class="s-btn" href="">
+          <i class="fab fa-searchengin"></i>
+          </a>
         </div>
             <?php
             require __DIR__ . '/vendor/autoload.php';

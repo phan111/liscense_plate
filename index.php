b7466dd91d39593a71dbb948d2149fbac999dd02
phan111 <chatpimookt@gmail.com>
2022-01-10 15:48:11 +0700

    if not found

--- index.php
+++ index.php
@@ -49,25 +49,29 @@
             $response = $service->spreadsheets_values->get($spreadsheetId, $get_range);
             $values = $response->getValues();
             if(!empty($_GET)){
-                foreach($values AS $key){
-                    //print_r($key[7]);
-                    //print_r(strpos($key[7], $search));
-                    if(strpos($key[7], $_GET['search'])){
-						if(strpos($key[10], "ไม่") === FALSE){
-                        	echo "<center style='padding-top:10px;font-size:25px'><b>".$key[2]."<br>(".$key[7].") <a href='tel:".$key[3]."'><i class='fa fa-phone fa-2x' style='color:#83FF33;padding-left15px'></i></a><hr style='margin-top:10px'></b></center>";
-						}else{
-							$get_range = "emp!A2:F";
-							$emp_sheet = $service->spreadsheets_values->get($spreadsheetId, $get_range);
-							foreach($emp_sheet->getValues() AS $emp){
-								if($emp[0] == $key[1]){
-									$department = $emp[5];
+				if(!empty($values)){
+					foreach($values AS $key){
+						//print_r($key[7]);
+						//print_r(strpos($key[7], $search));
+						if(strpos($key[7], $_GET['search'])){
+							if(strpos($key[10], "ไม่") === FALSE){
+								echo "<center style='padding-top:10px;font-size:25px'><b>".$key[2]."<br>(".$key[7].") <a href='tel:".$key[3]."'><i class='fa fa-phone fa-2x' style='color:#83FF33;padding-left15px'></i></a><hr style='margin-top:10px'></b></center>";
+							}else{
+								$get_range = "emp!A2:F";
+								$emp_sheet = $service->spreadsheets_values->get($spreadsheetId, $get_range);
+								foreach($emp_sheet->getValues() AS $emp){
+									if($emp[0] == $key[1]){
+										$department = $emp[5];
+									}
 								}
+								echo "<center style='padding-top:10px;font-size:25px'><b>".$key[2]."<br>(".$key[7].")<br>" .$department."<br>เบอร์ภายใน:".$key[5]."<hr style='margin-top:10px'></b></center>";
 							}
-							echo "<center style='padding-top:10px;font-size:25px'><b>".$key[2]."<br>(".$key[7].")<br>" .$department."<br>เบอร์ภายใน:".$key[5]."<hr style='margin-top:10px'></b></center>";
+	
 						}
-
-                    }
-                }
+					}
+				}else{
+					echo "<center>ไม่พบข้อมูล</center>";
+				}
             }
             ?>
 

08c07e2e7d1d781416032b4b206dbbc79202c19c
phan111 <chatpimookt@gmail.com>
2022-01-13 14:06:24 +0700

    spd test

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<title>ค้นหาทะเบียนรถ กฟต.1</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 </head>
+
 <body>
 
 
@@ -34,47 +36,45 @@
 					<input type="submit" class="contact100-form-btn" value="ค้นหา">
 				</div>
 			</form>
-	<?php
-            require __DIR__ . '/vendor/autoload.php';
-            //Reading data from spreadsheet.
-            $client = new \Google_Client();
-            $client->setApplicationName('Google Sheets and PHP');
-            $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
-            $client->setAccessType('offline');
-            $client->setAuthConfig(__DIR__ . '/credentials.json');
-            $service = new Google_Service_Sheets($client);
-            $spreadsheetId = "18YfUWVh4UWmD_xbNOy1FaT-mlnjjzgo8ov-dfeIsCWE"; //It is present in your URL
-            $get_range = "B2:B,D2:D,F2:F,H2:H,K2:K";
-            //Request to get data from spreadsheet.
-            $response = $service->spreadsheets_values->get($spreadsheetId, $get_range);
-            $values = $response->getValues();
-			echo "<pre>"; print_r($values);
+			<?php
+			require __DIR__ . '/vendor/autoload.php';
+			//Reading data from spreadsheet.
+			$client = new \Google_Client();
+			$client->setApplicationName('Google Sheets and PHP');
+			$client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+			$client->setAccessType('offline');
+			$client->setAuthConfig(__DIR__ . '/credentials.json');
+			$service = new Google_Service_Sheets($client);
+			$spreadsheetId = "18YfUWVh4UWmD_xbNOy1FaT-mlnjjzgo8ov-dfeIsCWE"; //It is present in your URL
+			$get_range = "B2:K";
+			//Request to get data from spreadsheet.
+			$response = $service->spreadsheets_values->get($spreadsheetId, $get_range);
+			$values = $response->getValues();
 			$found = 0;
-            /*if(!empty($_GET)){
-                foreach($values AS $key){
-                    //print_r($key[7]);
-                    //print_r(strpos($key[7], $search));
-						if(strpos($key[7], $_GET['search'])){
-							$found++;
-						if(strpos($key[10], "ไม่") === FALSE){
-                        	echo "<center style='padding-top:10px;font-size:25px'><b>".$key[2]."<br>(".$key[7].") <a href='tel:".$key[3]."'><i class='fa fa-phone fa-2x' style='color:#83FF33;padding-left15px'></i></a><hr style='margin-top:10px'></b></center>";
-						}else{
+			echo "<pre>";print_r($values);
+			if (!empty($_GET)) {
+				foreach ($values as $key) {
+					if (strpos($key[7], $_GET['search'])) {
+						$found++;
+						if (strpos($key[10], "ไม่") === FALSE) {
+							echo "<center style='padding-top:10px;font-size:25px'><b>" . $key[2] . "<br>(" . $key[7] . ") <a href='tel:" . $key[3] . "'><i class='fa fa-phone fa-2x' style='color:#83FF33;padding-left15px'></i></a><hr style='margin-top:10px'></b></center>";
+						} else {
 							$get_range = "emp!A2:F";
 							$emp_sheet = $service->spreadsheets_values->get($spreadsheetId, $get_range);
-							foreach($emp_sheet->getValues() AS $emp){
-								if($emp[0] == $key[1]){
+							foreach ($emp_sheet->getValues() as $emp) {
+								if ($emp[0] == $key[1]) {
 									$department = $emp[5];
 								}
 							}
-							echo "<center style='padding-top:10px;font-size:25px'><b>".$key[2]."<br>(".$key[7].")<br>" .$department."<br>เบอร์ภายใน:".$key[5]."<hr style='margin-top:10px'></b></center>";
+							echo "<center style='padding-top:10px;font-size:25px'><b>" . $key[2] . "<br>(" . $key[7] . ")<br>" . $department . "<br>เบอร์ภายใน:" . $key[5] . "<hr style='margin-top:10px'></b></center>";
 						}
-                    }
-                }
-				if($found == 0){
+					}
+				}
+				if ($found == 0) {
 					echo "<center><b style='color:red'>ไม่พบข้อมูล</b></center>";
 				}
-            }*/
-            ?>
+			}
+			?>
 
 		</div>
 
@@ -82,4 +82,5 @@
 
 
 </body>
+
 </html>
